54728717f08faae0b433bd9013e3ae9dd4e2bb54
karahalil <karahaliloglu.cevre@gmail.com>
2023-07-19 17:02:01 +0300

    animated menu

---
+++ proje-3 animated menu/app.js
@@ -0,0 +1,11 @@
+const open = document.getElementById('open')
+const close = document.getElementById('close')
+const container = document.querySelector('.container')
+
+
+function acıl(){
+    container.classList.add('show-nav')
+}
+function kapan(){
+    container.classList.remove('show-nav')
+}
