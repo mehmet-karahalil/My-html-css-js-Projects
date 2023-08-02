93b84c7a3d8f9ba5a0e758cf0683d2d7f90e8b7e
mehmet-karahalil <mehmetemin.karahaliloglu@gmail.com>
2023-08-03 02:30:36 +0300

    pokedex updated

--- proje-20 pokedex/app.js
+++ proje-20 pokedex/app.js
@@ -2,9 +2,8 @@ var poke_container = document.querySelector(".poke-container");
 var search = document.querySelector(".search");
 var searchbtn = document.querySelector(".searchbtn");
 var searchinput = document.querySelector(".searchinput");
-var pokemon_count = 150;
-
-
+var selectElement = document.getElementById("select1")
+var pokemon_count = 1011;
 
 const bg_color = {
     grass: '#8BD369',
@@ -24,40 +23,69 @@ const bg_color = {
     ice: '#66CCFF',
 }
 
-searchbtn.addEventListener("click", () =>{
+searchbtn.addEventListener("click", () => {
     search.classList.toggle("active");
 })
 
-searchinput.addEventListener("input", ()=>{
-    var searchvalue = searchinput.value.toLowerCase();
-    var pokemonnames = document.querySelectorAll(".poke-name");
-    pokemonnames.forEach((pokename) =>{
-        if(pokename.innerHTML.toLowerCase().includes(searchvalue)) {
-            pokename.parentElement.parentElement.parentElement.style.display = "block";
-        }else{
-            pokename.parentElement.parentElement.parentElement.style.display = "none";
-        }
-    })
+searchinput.addEventListener("input", () => {
+
+    if (/^\d+$/.test(searchinput.value) == false) {
+        var searchvalue = searchinput.value.toLowerCase();
+        var pokemonnames = document.querySelectorAll(".poke-name");
+        pokemonnames.forEach((pokename) => {
+            if (pokename.innerHTML.toLowerCase().includes(searchvalue)) {
+                pokename.parentElement.parentElement.parentElement.style.display = "block";
+            } else {
+                pokename.parentElement.parentElement.parentElement.style.display = "none";
+            }
+        })
+    }
+    else {
+        var searchvalue = searchinput.value
+        var pokemonids = document.querySelectorAll(".poke-id");
+        pokemonids.forEach((pokeid) => {
+            if (pokeid.innerHTML.includes(searchvalue)) {
+                pokeid.parentElement.parentElement.parentElement.style.display = "block";
+            } else {
+                pokeid.parentElement.parentElement.parentElement.style.display = "none";
+            }
+        })
+    }
 })
 
-var fetchpokemon = async()=>{
+var fetchpokemon = async () => {
     for (let index = 1; index < pokemon_count; index++) {
         await getpokemon(index);
     }
 }
 
-var getpokemon = async(id) =>{
+var getpokemon = async (id) => {
     var url = `https://pokeapi.co/api/v2/pokemon/${id}`;
     var res = await fetch(url);
     var data = await res.json();
     createpokemoncard(data);
 }
 
-var createpokemoncard = (pokemon) => {
-    var pokemondiv  = document.createElement("div");
-    pokemondiv.classList.add("pokemon");
-    pokemondiv.classList.add("pokemon");
+selectElement.addEventListener('change', function () {
+    var selectedValue = selectElement.value;
+    var pokeTypes = document.querySelectorAll("#poketip");
 
+    pokeTypes.forEach((pokeType) => {
+        var pokemonCard = pokeType.parentElement.parentElement.parentElement.parentElement.parentElement;
+
+        if (selectedValue == pokeType.textContent.toLowerCase()) {
+            pokemonCard.style.display = "block";
+        } else if (selectedValue == "#") {
+            pokemonCard.style.display = "block";
+        } else {
+            pokemonCard.style.display = "none";
+        }
+    });
+});
+
+var createpokemoncard = (pokemon) => {
+    var pokemondiv = document.createElement("div");
+    pokemondiv.classList.add("pokemon");
     var pokemonid = pokemon.id
     var pokemontype = pokemon.types[0].type.name;
     var pokemonbg = bg_color[pokemontype]
@@ -79,7 +107,7 @@ var createpokemoncard = (pokemon) => {
                     </small>
                 </div>
                 <div class="poke-type">
-                    <i class="fa-brands fa-uncharted">${pokemontype}</i>
+                    <i class="fa-brands fa-uncharted"><span id="poketip">${pokemontype}</span></i>
                 </div>
             </div>
         </div>
@@ -89,11 +117,3 @@ var createpokemoncard = (pokemon) => {
 }
 
 fetchpokemon()
-
-
-
-
-
-
-
-
